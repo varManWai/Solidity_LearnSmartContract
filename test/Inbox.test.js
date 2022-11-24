5eea8f9f97d86d56cf4677318d528f7ebdc9514e
varManWai <laizoke98@gmail.com>
2022-11-24 23:53:38 +0800

    added the web3 instance and the hosting network we will use in the para of it

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,4 +4,4 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');  //Web3 need to use capital letter because it is a "constructor"
-
+const web3 = new Web3(ganache.provider()); //it is lower case because it is an instance // ganache.provider() is to tell the hosting network we used in the machine 
