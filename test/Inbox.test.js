6a79bc39eaa88eee8ea56ad5c22097ff73de4d8a
varManWai <laizoke98@gmail.com>
2022-11-24 23:42:13 +0800

    added the basic import/requires for the testing in inbox.test.js file

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,3 +1,7 @@
 // contract test code will go here
 
 //functional test
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');  //Web3 need to use capital letter because it is a "constructor"
+
