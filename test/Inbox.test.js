b4cd2a746952704ce364d7a3300d55a53d924514
varManWai <laizoke98@gmail.com>
2022-11-25 01:39:36 +0800

    added and understand the main functon of the mocha testing

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,22 +3,36 @@
 //functional test
 const assert = require("assert");
 const ganache = require("ganache-cli");
+const { beforeEach } = require("mocha");
 const Web3 = require("web3"); //Web3 need to use capital letter because it is a "constructor"
 const web3 = new Web3(ganache.provider()); //it is lower case because it is an instance // ganache.provider() is to tell the hosting network we used in the machine
 
 class Car {
-  park() { //method 1
+  park() {
+    //method 1
     return "stopped";
   }
 
-  drive() { //method 2
+  drive() {
+    //method 2
     return "vroom";
   }
 }
 
-describe("Car", () => { //Car just the name 
-  it("can drive", () => { //can drive also just a words
-    const car = new Car();
+let car;
+
+beforeEach(() => { //run before the describe 
+  car = new Car();
+});
+
+describe("Car", () => {
+  //Car just the name
+  it("can drive", () => {
+    //can drive also just a words
     assert.equal(car.park(), "stopped"); // compare the car.park() return the value same as the second para
+  });
+
+  it("can drive", () => {
+    assert.equal(car.drive(), "vroom");
   });
 });
