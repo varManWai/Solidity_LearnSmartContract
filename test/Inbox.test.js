6f2f81d2377f894852157a2f3f441ed1f8c98603
varManWai <laizoke98@gmail.com>
2022-11-24 23:59:42 +0800

    added the example of the testing code in inbox.test.js

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,24 @@
 // contract test code will go here
 
 //functional test
-const assert = require('assert');
-const ganache = require('ganache-cli');
-const Web3 = require('web3');  //Web3 need to use capital letter because it is a "constructor"
-const web3 = new Web3(ganache.provider()); //it is lower case because it is an instance // ganache.provider() is to tell the hosting network we used in the machine 
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3"); //Web3 need to use capital letter because it is a "constructor"
+const web3 = new Web3(ganache.provider()); //it is lower case because it is an instance // ganache.provider() is to tell the hosting network we used in the machine
+
+class Car {
+  park() {
+    return "stopped";
+  }
+
+  drive() {
+    return "vroom";
+  }
+}
+
+describe("Car", () => {
+  it("can drive", () => {
+    const car = new Car();
+    assert.equal(car.park(), "stopped");
+  });
+});
