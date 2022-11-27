f0bba6138abea7bc9daf4cf6937cbf993a6948e7
varManWai <laizoke98@gmail.com>
2022-11-27 15:09:28 +0800

    changed somthing small

--- deploy.js
+++ deploy.js
@@ -2,28 +2,32 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
 const { interface, bytecode } = require("./compile");
 
-const provider = new HDWalletProvider(
-  
-);
+const provider = new HDWalletProvider();
 
 const web3 = new Web3(provider);
 
 const deploy = async () => {
+  const dotenv = require("dotenv");
+  dotenv.config();
+
   const accounts = await web3.eth.getAccounts();
 
-  console.log("Attempting to deploy from account", accounts[0]);
+  console.log("Attempting to deploy from account", accounts[2]);
 
-  try {
-    const result = await new web3.eth.Contract(JSON.parse(interface))
-      .deploy({ data: bytecode, arguments: ["Hi there!"] })
-      .send({ gas: "1000000", from: accounts[2] });
+  console.log(process.env.MNE);
+  console.log(process.env.INFURA);
 
-    console.log("Contract deployed to", result.options.address);
-  
-  } catch (err) {
-    console.log(err);
+  // try {
+  //   const result = await new web3.eth.Contract(JSON.parse(interface))
+  //     .deploy({ data: bytecode, arguments: ["Hi there!"] })
+  //     .send({ gas: "1000000", from: accounts[2] });
 
-  }
+  //   console.log("Contract deployed to", result.options.address);
+
+  // } catch (err) {
+  //   console.log(err);
+
+  // }
 
   provider.engine.stop();
 };
