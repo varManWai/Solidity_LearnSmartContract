15fb125d11919efc02c3fd6e249e5fe9b7a46a4a
varManWai <laizoke98@gmail.com>
2022-11-27 15:24:34 +0800

    completed

--- deploy.js
+++ deploy.js
@@ -1,33 +1,27 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
 const { interface, bytecode } = require("./compile");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const provider = new HDWalletProvider();
+const provider = new HDWalletProvider(process.env.MNE, process.env.INFURA);
 
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-  const dotenv = require("dotenv");
-  dotenv.config();
+  try {
+    const accounts = await web3.eth.getAccounts();
 
-  const accounts = await web3.eth.getAccounts();
+    console.log("Attempting to deploy from account", accounts[2]);
 
-  console.log("Attempting to deploy from account", accounts[2]);
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({ data: bytecode, arguments: ["Hi there!"] })
+      .send({ gas: "1000000", from: accounts[2] });
 
-  console.log(process.env.MNE);
-  console.log(process.env.INFURA);
-
-  // try {
-  //   const result = await new web3.eth.Contract(JSON.parse(interface))
-  //     .deploy({ data: bytecode, arguments: ["Hi there!"] })
-  //     .send({ gas: "1000000", from: accounts[2] });
-
-  //   console.log("Contract deployed to", result.options.address);
-
-  // } catch (err) {
-  //   console.log(err);
-
-  // }
+    console.log("Contract deployed to", result.options.address);
+  } catch (err) {
+    console.log(err);
+  }
 
   provider.engine.stop();
 };
